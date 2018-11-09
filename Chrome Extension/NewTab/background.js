e16ef67de03bbb6e1672fc6c31b7e2421d2db49c
Tomer Ben Rachel <tomerpacific@gmail.com>
2018-11-09 13:13:57 +0200

    Renaming background script and making changes in manifest

--- Chrome Extension/NewTab/background.js
+++ Chrome Extension/NewTab/background.js
@@ -1,9 +1,3 @@
-//chrome.tabs.create({url: 'popup.html'}) 
-
-// chrome.tabs.create({'url': chrome.extension.getURL('popup.html')}, function(tab) {
-//     // Tab opened.
-// });
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.create({'url': chrome.extension.getURL('popup.html')}, function(tab) {
 		// Tab opened.
