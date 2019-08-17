386ee55511f001d8c907d800a3abda0f1cc7a76f
unknown <tomerpacific@gmail.com>
2019-08-17 20:57:18 +0300

    MediumArticles AndroidStorage adding internal storage project

---
+++ AndroidStorage/Internal/app/src/main/java/com/tomerpacific/androidinternalstorage/MainActivity.java
@@ -0,0 +1,47 @@
+package com.tomerpacific.androidinternalstorage;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String FILENAME = "hello_world_file";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void saveFileInternalStorage(View view) {
+
+        String inputToFile = "Hello From Internal Storage!";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fileOutputStream.write(inputToFile.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getApplicationContext(),
+                    "File " + FILENAME + " has been saved successfully",
+                    Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "File " + FILENAME + " has not been saved successfully due to an exception " + e.getLocalizedMessage(),
+                    Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),
+                    "File " + FILENAME + " has not been saved successfully due to an exception " + e.getLocalizedMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
