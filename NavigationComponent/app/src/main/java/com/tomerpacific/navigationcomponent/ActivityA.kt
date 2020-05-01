bfdb7dc0bb17885018bb60578d7777556098a595
unknown <tomerpacific@gmail.com>
2020-05-01 16:03:07 +0300

    MediumArticles NavigationComponent project

---
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/ActivityA.kt
@@ -0,0 +1,12 @@
+package com.tomerpacific.navigationcomponent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityA : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_a)
+    }
+}
---
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/ActivityB.kt
@@ -0,0 +1,12 @@
+package com.tomerpacific.navigationcomponent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityB : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+    }
+}
