037240a1a7007d86ed563162944e01945730e8ee
unknown <tomerpacific@gmail.com>
2020-05-01 22:53:20 +0300

    MediumArticles NavigationComponent replacing activities with fragments

---
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/FragmentA.kt
@@ -0,0 +1,18 @@
+package com.tomerpacific.navigationcomponent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentA: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+}
--- NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.tomerpacific.navigationcomponent
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -11,17 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val activityABtn : Button = findViewById(R.id.activty_a_btn)
-        val activityBBtn : Button = findViewById(R.id.activity_b_btn)
+        val activityABtn : Button = findViewById(R.id.fragment_a_btn)
+        val activityBBtn : Button = findViewById(R.id.fragment_b_btn)
 
         activityABtn.setOnClickListener {button ->
-            val intentA = Intent(this, ActivityA::class.java)
-            startActivity(intentA)
+            val myFragment : FragmentA = FragmentA()
+            supportFragmentManager.beginTransaction().replace(R.id.container, myFragment).addToBackStack(null).commit()
         }
 
         activityBBtn.setOnClickListener {button ->
-            val intentA = Intent(this, ActivityB::class.java)
-            startActivity(intentA)
+            val myFragment : FragmentB = FragmentB()
+            supportFragmentManager.beginTransaction().replace(R.id.container, myFragment).addToBackStack(null).commit()
         }
     }
 }
