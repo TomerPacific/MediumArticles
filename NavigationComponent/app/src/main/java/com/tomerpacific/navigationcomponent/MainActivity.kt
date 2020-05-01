bc9aa05448c353b1a5018470c188299065bd1340
unknown <tomerpacific@gmail.com>
2020-05-01 17:02:16 +0300

    MediumArticles NavigationComponent logic to go to other activities

--- NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.tomerpacific.navigationcomponent
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val activityABtn : Button = findViewById(R.id.activty_a_btn)
+        val activityBBtn : Button = findViewById(R.id.activity_b_btn)
+
+        activityABtn.setOnClickListener {button ->
+            val intentA = Intent(this, ActivityA::class.java)
+            startActivity(intentA)
+        }
+
+        activityBBtn.setOnClickListener {button ->
+            val intentA = Intent(this, ActivityB::class.java)
+            startActivity(intentA)
+        }
     }
 }
