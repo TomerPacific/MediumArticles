5ec404229c1ef39c6db1d940c671372d3a174716
unknown <tomerpacific@gmail.com>
2020-05-02 16:54:36 +0300

    MediumArticles removing navigation fragment and refactoring fragment logic

--- NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
+++ NavigationComponent/app/src/main/java/com/tomerpacific/navigationcomponent/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myFragment : StartFragment = StartFragment()
+        supportFragmentManager.beginTransaction().add(R.id.container, myFragment).addToBackStack(null).commit()
     }
 }
