0912574a1fc2d9775a603c54afafe30d8a84cc67
unknown <tomerpacific@gmail.com>
2020-11-27 23:43:25 +0200

    MediumArticles adding flutter project for communication

---
+++ flutter_communication_bridge/android/app/src/main/kotlin/stackoverflow/com/tomerpacific/flutter_communication_bridge/MainActivity.kt
@@ -0,0 +1,6 @@
+package stackoverflow.com.tomerpacific.flutter_communication_bridge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_communication_bridge/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Communication Bridge',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Native - JS Communication Bridge'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  WebViewController _controller;
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(title: Text('Webview')),
+      body: WebView(
+        initialUrl: 'about:blank',
+        javascriptMode: JavascriptMode.unrestricted,
+        javascriptChannels: Set.from([
+          JavascriptChannel(
+              name: 'messageHandler',
+              onMessageReceived: (JavascriptMessage message) {
+                _scaffoldKey.currentState.showSnackBar(
+                    SnackBar(
+                        content: Text(
+                            message.message)
+                    )
+                );
+              })
+        ]),
+        onWebViewCreated: (WebViewController webviewController) {
+          _controller = webviewController;
+          _loadHtmlFromAssets();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_upward),
+        onPressed: () {
+          _controller.evaluateJavascript('fromFlutter("From Flutter")');
+        },
+      ),
+    );
+
+  }
+
+  _loadHtmlFromAssets() async {
+    String file = await rootBundle.loadString('assets/index.html');
+    _controller.loadUrl(Uri.dataFromString(
+        file,
+        mimeType: 'text/html',
+        encoding: Encoding.getByName('utf-8')).toString());
+  }
+
+}
