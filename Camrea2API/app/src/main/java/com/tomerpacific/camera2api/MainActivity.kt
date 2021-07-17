f00dafb97da981d67f2399be1ee58e5ed458808e
unknown <tomerpacific@gmail.com>
2021-07-17 16:54:33 +0300

    MediumArticles Camera2API adding surfaceTextureListener and method to get cameraId

--- Camrea2API/app/src/main/java/com/tomerpacific/camera2api/MainActivity.kt
+++ Camrea2API/app/src/main/java/com/tomerpacific/camera2api/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.tomerpacific.camera2api
 
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.TextureView
@@ -7,11 +11,54 @@ import android.view.TextureView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textureView: TextureView
+    private lateinit var cameraId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textureView = findViewById(R.id.texture_view)
+
+        getRearFacingCameraId()
+    }
+
+
+    private fun getRearFacingCameraId() {
+        val cameraManager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraIds: Array<String> = cameraManager.cameraIdList
+
+        for (id in cameraIds) {
+            val cameraCharacteristics = cameraManager.getCameraCharacteristics(id)
+
+            //If we want to choose the rear facing camera instead of the front facing one
+            if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
+                continue
+            }
+
+            cameraId = id
+        }
+    }
+
+
+
+    /**
+     * Surface Texture Listener
+     */
+
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(texture: SurfaceTexture, width: Int, height: Int) {
+
+        }
+        override fun onSurfaceTextureSizeChanged(texture: SurfaceTexture, width: Int, height: Int) {
+
+        }
+
+        override fun onSurfaceTextureDestroyed(texture: SurfaceTexture): Boolean {
+            return true
+        }
+
+        override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {
+
+        }
     }
 }
