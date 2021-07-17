07949a271708f8e059df4919d1c77dee95b50858
unknown <tomerpacific@gmail.com>
2021-07-17 17:01:57 +0300

    MediumArticles Camera2API adding backgroundThread related logic, cameraStateCallback, and openCamera

--- Camrea2API/app/src/main/java/com/tomerpacific/camera2api/MainActivity.kt
+++ Camrea2API/app/src/main/java/com/tomerpacific/camera2api/MainActivity.kt
@@ -1,30 +1,44 @@
 package com.tomerpacific.camera2api
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import android.view.TextureView
+
+val TAG = MainActivity::class.simpleName
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textureView: TextureView
     private lateinit var cameraId: String
+    private lateinit var backgroundHandlerThread: HandlerThread
+    private lateinit var backgroundHandler: Handler
+    private lateinit var cameraManager: CameraManager
 
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textureView = findViewById(R.id.texture_view)
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         getRearFacingCameraId()
+        cameraManager.openCamera(cameraId, cameraStateCallback, backgroundHandler)
     }
 
 
     private fun getRearFacingCameraId() {
-        val cameraManager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
         val cameraIds: Array<String> = cameraManager.cameraIdList
 
         for (id in cameraIds) {
@@ -60,5 +74,46 @@ class MainActivity : AppCompatActivity() {
         override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {
 
         }
+    }
+
+    /**
+     * Camera State Callbacks
+     */
+
+    private val cameraStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+
+        }
+
+        override fun onDisconnected(cameraDevice: CameraDevice) {
+
+        }
+
+        override fun onError(cameraDevice: CameraDevice, error: Int) {
+            val errorMsg = when(error) {
+                ERROR_CAMERA_DEVICE -> "Fatal (device)"
+                ERROR_CAMERA_DISABLED -> "Device policy"
+                ERROR_CAMERA_IN_USE -> "Camera in use"
+                ERROR_CAMERA_SERVICE -> "Fatal (service)"
+                ERROR_MAX_CAMERAS_IN_USE -> "Maximum cameras in use"
+                else -> "Unknown"
+            }
+            Log.e(TAG, "Error when trying to connect camera $errorMsg")
+        }
+    }
+
+    /**
+     * Background Thread
+     */
+    private fun startBackgroundThread() {
+        backgroundHandlerThread = HandlerThread("CameraVideoThread")
+        backgroundHandlerThread.start()
+        backgroundHandler = Handler(
+                backgroundHandlerThread.looper)
+    }
+
+    private fun stopBackgroundThread() {
+        backgroundHandlerThread.quitSafely()
+        backgroundHandlerThread.join()
     }
 }
