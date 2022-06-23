f92573f03b5e4fb965c020580d7b22fd55ca5023
unknown <tomerpacific@gmail.com>
2022-06-23 20:29:57 +0300

    MediumArticles initial project commit

---
+++ alarm_manager_example/android/app/src/main/kotlin/com/tomerpacific/alarm_manager_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tomerpacific.alarm_manager_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ alarm_manager_example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alarm Manager Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Alarm Manager Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+              Text(
+              'Alarm Manager Example',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
