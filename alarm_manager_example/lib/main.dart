760d4d4f167e6a35b84c9aeaee2e4e3848225ff3
unknown <tomerpacific@gmail.com>
2022-06-23 22:45:57 +0300

    MediumArticles AlarmManager initializing plugin

--- alarm_manager_example/lib/main.dart
+++ alarm_manager_example/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AndroidAlarmManager.initialize();
   runApp(const MyApp());
 }
 
