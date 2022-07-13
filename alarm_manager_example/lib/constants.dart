f2976e4a3bd8a158b468cc3b3347f03933611bd1
unknown <tomerpacific@gmail.com>
2022-07-13 20:42:21 +0300

    MediumArticles AlarmManager creating constants class

---
+++ alarm_manager_example/lib/constants.dart
@@ -0,0 +1,9 @@
+
+  const String appName = "Alarm Manager Example";
+  const String durationSeconds = "Seconds";
+  const String durationMinutes = "Minutes";
+  const String durationHours = "Hours";
+  const String oneShotAlarm = "oneShot";
+  const String oneShotAtAlarm = "oneShotAt";
+  const String periodicAlarm = "periodic";
+
