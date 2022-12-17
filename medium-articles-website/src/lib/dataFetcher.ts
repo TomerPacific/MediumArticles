f0145235d1028f9bbab4ab38e5421ac981948d12
unknown <tomerpacific@gmail.com>
2022-12-17 15:12:49 +0200

    MediumArticles website adding logic when fetching items

--- medium-articles-website/src/lib/dataFetcher.ts
+++ medium-articles-website/src/lib/dataFetcher.ts
@@ -1,8 +1,20 @@
-import type { Article } from './Article'
+import type { Article } from './article'
 
 
 export function gatherMediumArticles(): Article[] {
     let articles: Article[]
-    
-    return articles
+
+    fetch('https://github.com/TomerPacific/MediumArticles/blob/master/README.md')
+    .then(function(response) {
+        return response.text()
+    })
+    .then(function(html) {
+        console.log(html)
+        return articles
+    }).catch(function(e) {
+        console.error(e)
+        return []
+    })
+
+    return []
 }
