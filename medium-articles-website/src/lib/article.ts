98785e40f2ff8250602b90385779cf156de70c3e
unknown <tomerpacific@gmail.com>
2022-12-19 22:19:40 +0200

    MediumArticles website changing constructor of article class

--- medium-articles-website/src/lib/article.ts
+++ medium-articles-website/src/lib/article.ts
@@ -2,9 +2,9 @@ export class Article {
     url:string;
     title: string;
 
-    constructor(json: any) {
-        this.url = json.url;
-        this.title = json.title;
+    constructor(title: string, link: string) {
+        this.url = link;
+        this.title = title;
     }
 
 }
