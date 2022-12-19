449074808746c2e43b79e1dffbfaee0348b9f062
unknown <tomerpacific@gmail.com>
2022-12-19 22:20:15 +0200

    MediumArticles website refactoring fetching of data using endpoint and regex

--- medium-articles-website/src/lib/dataFetcher.ts
+++ medium-articles-website/src/lib/dataFetcher.ts
@@ -1,18 +1,24 @@
-import type { Article } from './article'
+import { Article } from './article'
 
-export function gatherMediumArticles(): Article[] {
-    let articles: Article[]
-
-    fetch('https://raw.githubusercontent.com/tomerpacific/MediumArticles/master/README.md')
-    .then(function(response) {
-        return response.text()
+export function gatherMediumArticles(): Promise<Article[]> {
+    let articles: Article[] = []
+    return new Promise((resolve, reject) => {
+        fetch('https://raw.githubusercontent.com/tomerpacific/MediumArticles/master/README.md')
+        .then(function(response) {
+            return response.text()
+        })
+        .then(function(html) {
+            const regexp = new RegExp(/\[(.*?)\]\((.*?)\)/, 'g')
+            let matches
+            while ((matches = regexp.exec(html)) !== null) {
+                let articleTitle = matches[1]
+                let articleLink = matches[2]
+                let article: Article = new Article(articleTitle, articleLink)
+                articles.push(article);
+            }
+            return resolve(articles)
+        }).catch(function(e) {
+            return reject(e)
+        })
     })
-    .then(function(html) {
-        console.log(html)
-        return articles
-    }).catch(function(e) {
-        return []
-    })
-
-    return []
 }
