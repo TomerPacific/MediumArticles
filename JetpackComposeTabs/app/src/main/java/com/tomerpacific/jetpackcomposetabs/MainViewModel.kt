d43dc5b7a1fff08d82f18db16633c9b333742a3c
unknown <tomerpacific@gmail.com>
2023-02-20 23:25:43 +0200

    MediumArticles JetpackComposeTabs creating view model class

---
+++ JetpackComposeTabs/app/src/main/java/com/tomerpacific/jetpackcomposetabs/MainViewModel.kt
@@ -0,0 +1,24 @@
+package com.tomerpacific.jetpackcomposetabs
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val tabIndex: MutableLiveData<Int> = MutableLiveData(0)
+    val index: LiveData<Int> = tabIndex
+
+    fun updateTabIndexBasedOnSwipe(isSwipeToTheLeft: Boolean) {
+        tabIndex.value = when (isSwipeToTheLeft) {
+            true -> Math.floorMod(tabIndex.value!!.plus(1), 6)
+            false -> Math.floorMod(tabIndex.value!!.minus(1), 6)
+        }
+    }
+
+    fun updateTabIndex(i: Int) {
+        tabIndex.value = i
+    }
+
+}
