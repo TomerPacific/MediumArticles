a083929fb108855cd0784d3d9b95412792de46ee
unknown <tomerpacific@gmail.com>
2023-02-24 00:06:16 +0200

    MediumArticles JetpackComposeTabs moving draggable state to view model

--- JetpackComposeTabs/app/src/main/java/com/tomerpacific/jetpackcomposetabs/MainViewModel.kt
+++ JetpackComposeTabs/app/src/main/java/com/tomerpacific/jetpackcomposetabs/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.tomerpacific.jetpackcomposetabs
 
 import android.app.Application
+import androidx.compose.foundation.gestures.DraggableState
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -11,8 +12,16 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     val index: LiveData<Int> = tabIndex
     val tabs = listOf("Home", "About", "Settings")
 
-    fun updateTabIndexBasedOnSwipe(isSwipeToTheLeft: Boolean) {
-        tabIndex.value = when (isSwipeToTheLeft) {
+    var isDragToTheLeft: Boolean = false
+    private val state = DraggableState { delta ->
+        isDragToTheLeft = delta > 0
+    }
+
+    private val dragState = MutableLiveData<DraggableState>(state)
+    val st: LiveData<DraggableState> = dragState
+
+    fun updateTabIndexBasedOnSwipe() {
+        tabIndex.value = when (isDragToTheLeft) {
             true -> Math.floorMod(tabIndex.value!!.plus(1), tabs.size)
             false -> Math.floorMod(tabIndex.value!!.minus(1), tabs.size)
         }
