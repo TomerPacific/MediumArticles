59760e5e0fadc2e7c7722fc9a3fd5ae16c882912
unknown <tomerpacific@gmail.com>
2023-10-01 09:56:13 +0300

    MediumArticles LocationPermission refactoring logic when to show permission request

--- LocationPermissions/app/src/main/java/com/tomerpacific/locationpermissions/MainActivity.kt
+++ LocationPermissions/app/src/main/java/com/tomerpacific/locationpermissions/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -30,7 +31,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.style.TextAlign
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -76,15 +76,17 @@ class MainActivity : ComponentActivity() {
             val lifecycleOwner = LocalLifecycleOwner.current
             DisposableEffect(key1 = lifecycleOwner, effect = {
                 val observer = LifecycleEventObserver { _, event ->
-                    if (event == Lifecycle.Event.ON_START && !locationPermissionsAlreadyGranted) {
+                    if (event == Lifecycle.Event.ON_START &&
+                        !locationPermissionsAlreadyGranted &&
+                        !shouldShowPermissionRationale) {
                         launcher.launch(locationPermissions)
                     }
                 }
                 lifecycleOwner.lifecycle.addObserver(observer)
                 onDispose {
                     lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
                 }
-            }
             )
 
             val scope = rememberCoroutineScope()
@@ -98,9 +100,8 @@ class MainActivity : ComponentActivity() {
                     Scaffold(snackbarHost = {
                         SnackbarHost(hostState = snackbarHostState)
                     }) { contentPadding ->
-                        Text(modifier = Modifier.padding(contentPadding),
-                            text = "Location Permissions",
-                        textAlign = TextAlign.Center)
+                        Text(modifier = Modifier.padding(contentPadding).fillMaxWidth(),
+                            text = "Location Permissions")
                         if (shouldShowPermissionRationale) {
                             LaunchedEffect(Unit) {
                                 scope.launch {
@@ -113,7 +114,8 @@ class MainActivity : ComponentActivity() {
                                     when (userAction) {
                                         SnackbarResult.ActionPerformed -> {
                                             shouldShowPermissionRationale = false
-                                            openApplicationSettings()
+                                            launcher.launch(locationPermissions)
+                                            //openApplicationSettings()
                                         }
                                         SnackbarResult.Dismissed -> {
                                             shouldShowPermissionRationale = false
@@ -121,7 +123,11 @@ class MainActivity : ComponentActivity() {
                                     }
                                 }
                             }
+                            //Condition fits for when application before first asking for permissions and when user chooses never to ask for permissions
                         }
+//                        else if (!locationPermissionsAlreadyGranted && !shouldShowPermissionRationale) {
+//                            openApplicationSettings()
+//                        }
                     }
                 }
             }
