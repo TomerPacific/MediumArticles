21402664ac49e6555be4315fd6a7a1d87f7d8bd0
unknown <tomerpacific@gmail.com>
2024-01-20 20:13:28 +0200

    MediumArticles Serialization using constants

--- serialization_example/lib/models/doughnut.dart
+++ serialization_example/lib/models/doughnut.dart
@@ -1,3 +1,5 @@
+import 'package:serialization_example/constants.dart';
+
 class Doughnut {
   final String name;
   final String filling;
@@ -7,16 +9,16 @@ class Doughnut {
   Doughnut(this.name, this.filling, this.topping, this.price);
 
   Doughnut.fromJson(Map<String, dynamic> json)
-      : name = json['name'],
-        filling = json['filling'],
-        topping = json['topping'],
-        price = json['price'];
+      : name = json[DOUGHNUT_NAME_KEY],
+        filling = json[DOUGHNUT_FILLING_KEY],
+        topping = json[DOUGHNUT_TOPPING_KEY],
+        price = json[DOUGHNUT_PRICE_KEY];
 
   Map<String, dynamic> toJson() => {
-    'name' : name,
-    'filling' : filling,
-    'topping' : topping,
-    'price' : price
+    DOUGHNUT_NAME_KEY : name,
+    DOUGHNUT_FILLING_KEY : filling,
+    DOUGHNUT_TOPPING_KEY : topping,
+    DOUGHNUT_PRICE_KEY : price
   };
 
 
