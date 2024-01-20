9edaf1e22d9330c1f293cde53feeab87030d0edd
unknown <tomerpacific@gmail.com>
2024-01-20 20:04:55 +0200

    MediumArticles Serialization removing comment

--- serialization_example/lib/main.dart
+++ serialization_example/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:serialization_example/models/doughnut.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
