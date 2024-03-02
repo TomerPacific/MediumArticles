294d2ae0cf989127d409b167452f58cba88ec5a4
unknown <tomerpacific@gmail.com>
2024-03-02 20:15:40 +0200

    MediumArticles website constants for magic numbers

--- medium-articles-website/src/lib/dataFetcher.ts
+++ medium-articles-website/src/lib/dataFetcher.ts
@@ -1,6 +1,8 @@
 import { Article } from './article'
 
 const ENDPOINT: string = 'https://raw.githubusercontent.com/tomerpacific/MediumArticles/master/README.md'
+const ARTICLE_TITLE_INDEX: number = 1;
+const ARTICLE_LINK_INDEX: number = 2;
 
 export function gatherMediumArticles(): Promise<Article[]> {
     let articles: Article[] = []
@@ -13,8 +15,8 @@ export function gatherMediumArticles(): Promise<Article[]> {
             const regexp = new RegExp(/\[(.*?)\]\((.*?)\)/, 'g')
             let matches
             while ((matches = regexp.exec(html)) !== null) {
-                let articleTitle = matches[1]
-                let articleLink = matches[2]
+                let articleTitle = matches[ARTICLE_TITLE_INDEX]
+                let articleLink = matches[ARTICLE_LINK_INDEX]
                 let article: Article = new Article(articleTitle, articleLink)
                 articles.push(article);
             }
