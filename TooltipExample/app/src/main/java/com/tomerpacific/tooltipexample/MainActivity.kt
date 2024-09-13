54d9990b8b11fabc512060c1a7ae4acee29fc81c
unknown <tomerpacific@gmail.com>
2024-09-13 14:49:59 +0300

    MediumArticles removing boilerplate code

--- TooltipExample/app/src/main/java/com/tomerpacific/tooltipexample/MainActivity.kt
+++ TooltipExample/app/src/main/java/com/tomerpacific/tooltipexample/MainActivity.kt
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.tomerpacific.tooltipexample.ui.theme.TooltipExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BasicTooltip()
                 }
             }
         }
@@ -30,17 +28,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun BasicTooltip() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TooltipExampleTheme {
-        Greeting("Android")
-    }
 }
