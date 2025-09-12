fdb83779ba34d1c53b956780b1bcd464d7712e93
unknown <tomerpacific@gmail.com>
2025-09-12 11:29:20 +0300

    MediumArticles TooltipExample upgrading compile/target sdk versions

--- TooltipExample/app/build.gradle.kts
+++ TooltipExample/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.tomerpacific.tooltipexample"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tomerpacific.tooltipexample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
