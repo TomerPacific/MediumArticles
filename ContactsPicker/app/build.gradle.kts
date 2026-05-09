4b02b70722d26ce3d6f865959afbfbc44c613dda
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-05-09 22:09:25 +0300

    ContactsPicker adding viewmodel dependency

--- ContactsPicker/app/build.gradle.kts
+++ ContactsPicker/app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
