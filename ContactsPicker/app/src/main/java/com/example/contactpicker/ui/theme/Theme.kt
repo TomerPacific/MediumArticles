68a436bd2ab9e2530cf37c7522ace270ddd32866
Tomer Ben-Rachel <tomerpacific@gmail.com>
2026-05-19 18:51:51 +0300

    ContactsPicker removing unused import

--- ContactsPicker/app/src/main/java/com/example/contactpicker/ui/theme/Theme.kt
+++ ContactsPicker/app/src/main/java/com/example/contactpicker/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.contactpicker.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
